efcea97586cfcc5225240256a72bc49484bd575e
GlassToadstool <jacob.les.tabitha@gmail.com>
2026-04-05 12:30:33 +0100

    Started on death messages

---
+++ worlds/portal2/client/DeathMessages.py
@@ -0,0 +1,8 @@
+default_death_messages = [
+    "Long fall boots could not save {player_name}",
+    "{player_name} found the cake",
+    "{player_name} looked at the operational end of the Aperture Science Handheld Portal Device",
+    "{player_name} died of known side effects",
+    "{player_name} died of unknown side effects"
+]
+
